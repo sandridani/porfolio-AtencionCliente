5a29b8eb66830342b4a057e783ec394210e73220
sandridani <sandri.dani.g85@gmail.com>
2023-12-21 01:08:58 -0300

    creo q esta lista

---
+++ javascript/funciones.js
@@ -0,0 +1,15 @@
+function redireccionCV(){
+    window.open("https://drive.google.com/file/d/1oLpolP-zxI3V0EHver7jwyKMVZkJKohc/view?usp=sharing", "_blank");
+}
+
+
+
+const redireccionSoft = () =>{
+    window.open("https://drive.google.com/drive/folders/1Q2H9inVdMcGNiXldrlYFm_gz6UoN5mRo?usp=sharing", "_blank");
+}
+
+
+
+const redireccionProg = ()=>{
+    window.open("https://drive.google.com/drive/folders/1n3g8KomC2iub_kvN07B3Wz3yGBQH-Aiw?usp=sharing", "_blank");
+}
